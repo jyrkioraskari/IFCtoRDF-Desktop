7251e34b4a614091169b39f3d044ee98c0410693
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2019-09-02 16:12:23 +0300

    Third party licences listed

--- IFCtoRDF-Desktop/src/main/java/fi/ni/gui/fx/IFC2RDF_Desktop.java
+++ IFCtoRDF-Desktop/src/main/java/fi/ni/gui/fx/IFC2RDF_Desktop.java
@@ -1,24 +1,6 @@
-/*
- * The GNU Affero General Public License
- * 
- * Copyright (c) 2015 Jyrki Oraskari (Jyrki.Oraskari@aalto.fi / rkiorri@gmail.com)
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 
 /*
- * To compile this, Java 11 is needed. jfxrt.jar is included, so, the the plugin should not be mandatory
+ * jfxrt.jar is included, so, the the plugin should not be mandatory
  * but installing the http://www.eclipse.org/efxclipse/index.html and http://gluonhq.com/open-source/scene-builder/
  * make coding easier. 
  * 
